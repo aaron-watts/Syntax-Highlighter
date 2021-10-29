38706ea87cf77b812509eb8b4e12df13b9eaf5ab
aaron-watts <retrolasered@gmail.com>
2021-10-29 10:59:49 +0100

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,76 @@
 const highlightJS = document.querySelectorAll('pre.syntax-highlighted.js');
 
-const generalSyntaxRules = {
-    strings: {
-        type: 'string',
-        regex: /"(?:[^"\\]|\\.)*"|('(?:[^'\\]|\\.)*')|`(?:[^`\\]|\\.)*`/g,
+const javascriptSyntax = {
+    expressions: {
+        'functions': /\b([\w]+)\s*\(/gm,
+        'keyTermsHard': /\b(if|then|else|for|while|do|class|function|return|in|of|new|this|try|catch|const|let|var)\b/gi,
+        'keyTermsSoft': /\b(true|false|null|nil|class|implements|extends|end|void|return|in|of|new|this|try|catch|def|except)\b/gi,
+        'comments': /(?:^|[^\\])(\/\/.*)$|\/\*[\s\S]*?\*\//gm,
+        'classNames': /\s[A-Z]\w*/g,
+        'number': /[\b\W](-?\d+)\b/g,
+        'punctuationSoft': /[[\](){}:]+|(?<!&\w*);/g,
+        'punctuationHard': /[\+\*!|?]|(&gt;|&lt;|&amp;)|\B=\B|=(?!")/g,
+        'strings': /"(?:[^"\\]|\\.)*"|('(?:[^'\\]|\\.)*')|`(?:[^`\\]|\\.)*`/g
     },
-    commentsXX: {
-        type: 'comment',
-        regex: [
-            /(?:^|[^\\])(\/\/.*)$/gm,
-            /\/\*[\s\S]*?\*\//g,
-            /\/\*\*[\s\S]*?\*\//g,
-        ]
+    classes: {
+        'functions': 'fn',
+        'keyTermsHard': 'hard',
+        'keyTermsSoft': 'soft',
+        'comments': 'comment',
+        'classNames': 'class',
+        'number': 'int',
+        'punctuationSoft': 'soft',
+        'punctuationHard': 'hard',
+        'strings': 'string'
     },
-    comments: {
-        regex: /(?:^|[^\\])(\/\/.*)$|\/\*[\s\S]*?\*\//gm
+    parseChar: (html, type) => {
+        return html.replaceAll(javascriptSyntax.expressions[type],
+            `<span class="sh-${javascriptSyntax.classes[type]}">$&</span>`);
     },
-    classNames: {
-        regex: /\s[A-Z]\w*/g
-    },
-    number: {
-        regex: /[\b\W](-?\d+)\b/g
-    },
-    punctuationSoft: {
-        regex: /[[\](){}:]+|(?<!&\w*);/g
-    },
-    punctuationHard: {
-        regex: /[\+\*!|?]|(&gt;|&lt;|&amp;)|\B=\B|=(?!")/g
-    },
-    numberXXX: {
-        type: 'number',
-        regex: [
-            /[\b\W](-?((?:\d+\.\d+|\.\d+|\d+\.)(?:e[+-]?\d+)?)|\d+(?:e[+-]?\d+))/gi,
-            /[\b\W](-?\d+)(?!\.)\b/g,
-            /[\b\W](-?0x[A-F0-9]+)\b/gi,
-        ]
-    },
-    functions: {
-        regex: /\b([\w]+)\s*\(/gm
-    },
-    keyTermsHard: {
-        regex: /\b(if|then|else|for|while|do|class|function|return|in|of|new|this|try|catch|const|let|var)\b/gi
-    },
-    keyTermsSoft: {
-        regex: /\b(true|false|null|nil|class|implements|extends|end|void|return|in|of|new|this|try|catch|def|except)\b/gi
+    parse: (sample) => {
+        const parseString = sample.innerText;
+        let html = sample.innerHTML;
+
+        new Set(parseString.match(javascriptSyntax.expressions.comments))
+            .forEach(match => {
+                html = html.replaceAll(match,
+                    `<span class="sh-comment">$&</span>`);
+            });
+
+        new Set(parseString.match(javascriptSyntax.expressions.strings))
+            .forEach(match => {
+                html = html.replaceAll(match,
+                    `<span class="sh-string">$&</span>`);
+            });
+
+        new Set(parseString.match(javascriptSyntax.expressions.keyTermsHard))
+            .forEach(match => {
+                html = html.replaceAll(new RegExp(`\\b(${match})\\b`, 'g'),
+                    `<span class="sh-hard">$&</span>`);
+            });
+
+        new Set(parseString.match(javascriptSyntax.expressions.classNames))
+            .forEach(match => {
+                html = html.replaceAll(match,
+                    `<span class="sh-class">$&</span>`);
+            })
+
+        new Set(parseString.match(javascriptSyntax.expressions.functions))
+            .forEach(match => {
+                html = html.replaceAll(match,
+                    `<span class="sh-fn">${match.slice(0, -1)}</span>(`);
+            });
+
+        html = javascriptSyntax.parseChar(html, 'number');
+
+        html = javascriptSyntax.parseChar(html, 'punctuationHard');
+
+        html = javascriptSyntax.parseChar(html, 'punctuationSoft');
+
+        return html;
     }
-}
+};
 
 highlightJS.forEach(sample => {
-    const parseString = sample.innerText;
-    parseString.innerHTML = sample.innerHTML;
-
-    new Set(parseString.match(generalSyntaxRules.comments.regex))
-        .forEach(match => {
-            sample.innerHTML = sample.innerHTML.replaceAll(match,
-                `<span class="sh-comment">$&</span>`);
-        });
-
-    new Set(parseString.match(generalSyntaxRules.strings.regex))
-        .forEach(match => {
-            sample.innerHTML = sample.innerHTML.replaceAll(match,
-                `<span class="sh-string">$&</span>`);
-        });
-
-    new Set(parseString.match(generalSyntaxRules.keyTermsHard.regex))
-        .forEach(match => {
-            sample.innerHTML = sample.innerHTML.replaceAll(new RegExp(`\\b(${match})\\b`, 'g'),
-                `<span class="sh-hard">$&</span>`);
-        });
-
-    new Set(parseString.match(generalSyntaxRules.classNames.regex))
-        .forEach(match => {
-            console.log(match)
-            sample.innerHTML = sample.innerHTML.replaceAll(match,
-                `<span class="sh-class">$&</span>`);
-        })
-
-    new Set(parseString.match(generalSyntaxRules.functions.regex))
-        .forEach(match => {
-            sample.innerHTML = sample.innerHTML.replaceAll(match,
-                `<span class="sh-fn">${match.slice(0, -1)}</span>(`);
-        });
-
-    sample.innerHTML = sample.innerHTML.replaceAll(generalSyntaxRules.number.regex,
-        `<span class="sh-int">$&</span>`);
-
-    sample.innerHTML = sample.innerHTML.replaceAll(generalSyntaxRules.punctuationHard.regex,
-        `<span class="sh-hard">$&</span>`);
-
-    sample.innerHTML = sample.innerHTML.replaceAll(generalSyntaxRules.punctuationSoft.regex,
-        `<span class="sh-soft">$&</span>`);
+    sample.innerHTML = javascriptSyntax.parse(sample);
 });
